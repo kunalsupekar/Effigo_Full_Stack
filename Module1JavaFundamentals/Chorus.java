c7362f50f89fd24452bd7b5f68eb9bda4e8251b0
kunalsupekar <kunal.supekar@mitaoe.ac.in>
2025-01-21 15:36:16 +0530

    topics completed on 20-21/01/2025

--- Module1JavaFundamentals/Chorus.java
+++ Module1JavaFundamentals/Chorus.java
@@ -1,8 +1,22 @@
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 public class Chorus {
     public static void main(String[] args) {
         //singChorus();
-    String str=null;
-    System.out.println(str.equals(null));
+    // String str=null;
+    // System.out.println(str.equals(null));
+    int a=4;
+    Integer k=a;
+    System.out.println(k.getClass());
+    BigDecimal dec=new BigDecimal(100);
+
+    dec.add(dec);
+
+    System.out.println(dec);
+
 
     }
 
--- Module1JavaFundamentals/tempCodeRunnerFile.java
+++ Module1JavaFundamentals/tempCodeRunnerFile.java
@@ -1,2 +1,12 @@
+ // try {
+    //     FileInputStream fis=new FileInputStream("hello.txt");
+    //     Scanner sc=new Scanner(fis);
+    //     while(sc.hasNextLine()){
+    //         String line=sc.nextLine();
+    //         String[] words=line.split("--");
 
-    public static void main(String[] args) {
+    //         Arrays.stream(words).forEach(x->System.out.println(x));
+    //     }
+    // } catch (Exception e) {
+    //     // TODO: handle exception
+    // }
